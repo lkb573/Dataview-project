07743a08eb706006eadeb1ee85d4e8e1aaa6cab2
Bong0309 <lkb573@gmail.com>
2017-07-27 12:07:26 +0900

    test commit_170727

---
+++ src/main/java/kr/re/kitri/dataview/DataviewApplication.java
@@ -0,0 +1,13 @@
+package kr.re.kitri.dataview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataviewApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		SpringApplication.run(DataviewApplication.class, args);
+	}
+}
